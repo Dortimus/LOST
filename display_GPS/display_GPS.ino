bd1d852feef25d7ccd38a9610344b4e7eb2efdd9
Mohammadp67 <azampopal06@gmail.com>
2026-02-27 15:19:14 -0800

    Update display_GPS.ino

--- display_GPS/display_GPS.ino
+++ display_GPS/display_GPS.ino
@@ -6,14 +6,16 @@
 #include "Display.h"
 #include "SD_header.h"
 
+#define LED_PIN 4
 File GPSfile;
 File* GPSfile_p = &GPSfile;
+unsigned long lastRefresh = 0;
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
   Wire.setClock(400000);
-  
+  pinMode(LED_PIN, OUTPUT);
   display_init();
   init_mag();
   init_gps();
@@ -23,26 +25,35 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN_POWER), toggleFlagPower, FALLING);
   attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN_DISPLAY), updateFlagDisplay, FALLING);
   attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN_SD_SAVE), toggleFlagSDSave, FALLING);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN_CONNECTED), FlagDisplayChange, CHANGE);
   
-  Serial.println("System Ready");
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_25, 0);
 }
 
 void loop() {
-  if (myGNSS.checkUblox()) {
-    PVTUpdate();
-    if (fix_type >= 3) {
-      Serial.printf("CONNECTED (Sats: %d)\n", (int)myGNSS.getSIV());
-    } else {
-      Serial.println("SEARCHING...");
-    }
-    if (SDState == 1 && GPSfile) {
-      SD_saving(GPSfile_p);
-    }
+  if (powerState == 1) {
+    display.clearDisplay(); display.display();
+    if (SDState == 1) { GPSfile.flush(); GPSfile.close(); }
+    esp_deep_sleep_start();
   }
 
-  compassDegree = bmm350.getCompassDegree();
-  update_display(displayState);
-  SD_saving_init(GPSfile_p);
+  // GPS Update (background)
+  if (myGNSS.checkUblox()) {
+    PVTUpdate();
+    if (SDState == 1 && GPSfile) SD_saving(GPSfile_p);
+  }
 
-  delay(50); // High refresh rate for smooth compass
+  // Instant Button Sync
+  if (SDState_next != SDState) {
+    SDState = SDState_next;
+    if (SDState == 1) { SD_saving_init(GPSfile_p); digitalWrite(LED_PIN, HIGH); }
+    else { if (GPSfile) { GPSfile.flush(); GPSfile.close(); } digitalWrite(LED_PIN, LOW); }
+  }
+
+  // Refresh Screen at ~20 FPS (50ms)
+  if (millis() - lastRefresh > 50) {
+    batteryLevel = checkBatteryLevel();
+    update_display(displayState, displayConnect);
+    lastRefresh = millis();
+  }
 }
