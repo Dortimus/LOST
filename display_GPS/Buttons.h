2579a978b543c10ef5e54fb51b357023ae01bd50
Mohammadp67 <azampopal06@gmail.com>
2026-03-03 22:55:17 -0800

    Update Buttons.h

--- display_GPS/Buttons.h
+++ display_GPS/Buttons.h
@@ -3,31 +3,25 @@
 
 #include "esp_sleep.h"
 
-// --- PIN DEFINITIONS ---
 #define INTERRUPT_PIN_POWER   25
 #define INTERRUPT_PIN_DISPLAY 34 
 #define INTERRUPT_PIN_SD_SAVE 39 
 #define INTERRUPT_PIN_CONNECTED 27
 
 #define DEBOUNCE_TIME 250 
-#define NUM_DISPLAY_STATES 1 
-#define NUM_DISPLAY_STATES 2
+#define NUM_DISPLAY_STATES 3 // 0: Home, 1: Bike, 2: Car, 3: Compass
 
-// --- STATE VARIABLES ---
-volatile int powerState = 1;
+volatile int powerState = 0; 
 volatile int displayState = 0;
 volatile int displayConnect = 0;
 volatile int SDState = 0;      
 volatile int SDState_next = 0;
 
-// --- DEBOUNCE TIMERS ---
 volatile unsigned long last_power_time = 0;   
 volatile unsigned long last_display_time = 0;
 volatile unsigned long last_sd_time = 0;
 volatile unsigned long last_display_connected_time = 0;
-volatile unsigned long last_display_disconnected_time = 0;
 
-// --- INTERRUPT FUNCTIONS ---
 void IRAM_ATTR toggleFlagPower() {
   unsigned long now = millis();
   if (now - last_power_time > DEBOUNCE_TIME) {
@@ -54,24 +48,17 @@ void IRAM_ATTR toggleFlagSDSave() {
 }
 
 void IRAM_ATTR FlagDisplayChange() {
-  delay(500); // Your original delay for connection stability
   unsigned long now = millis();
   if (now - last_display_connected_time > DEBOUNCE_TIME) {
-    if (digitalRead(INTERRUPT_PIN_CONNECTED) == LOW) {
-      displayConnect = 1;
-      last_display_connected_time = now;
-    } else if (digitalRead(INTERRUPT_PIN_CONNECTED) == HIGH) {
-      displayConnect = 0;
-      last_display_disconnected_time = now;
-    }
+    displayConnect = (digitalRead(INTERRUPT_PIN_CONNECTED) == LOW) ? 1 : 0;
+    last_display_connected_time = now;
   }
 }
 
 void button_init() {
-  pinMode(INTERRUPT_PIN_POWER, INPUT);
-  pinMode(INTERRUPT_PIN_DISPLAY, INPUT);
-  pinMode(INTERRUPT_PIN_SD_SAVE, INPUT);
+  pinMode(INTERRUPT_PIN_POWER, INPUT_PULLUP);
+  pinMode(INTERRUPT_PIN_DISPLAY, INPUT_PULLUP);
+  pinMode(INTERRUPT_PIN_SD_SAVE, INPUT_PULLUP);
   pinMode(INTERRUPT_PIN_CONNECTED, INPUT_PULLUP);
 }
-
 #endif
